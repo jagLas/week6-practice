02f120e165b307fd11a2ebbaf498592d9d8ee804
jagLas <jglasher@gmail.com>
2023-01-17 12:32:04 -0500

    part 1 passes spec

--- practice-for-week-06-social-graph-practice/social.js
+++ practice-for-week-06-social-graph-practice/social.js
@@ -8,23 +8,42 @@ class SocialNetwork {
   }
 
   addUser(name) {
-    // Your code here
+    this.currentID++;
+    const user = {id: this.currentID, name: name}
+    this.users[this.currentID] = user;
+    this.follows[this.currentID] = new Set();
+    return user['id'];
   }
 
   getUser(userID) {
-    // Your code here
+    if (!this.users[userID]) {
+      return null;
+    }
+    return this.users[userID];
   }
 
   follow(userID1, userID2) {
-    // Your code here
+    if (!this.users[userID2] || !this.users[userID1]) {
+      return false;
+    }
+
+    this.follows[userID1].add(userID2);
+    return true;
   }
 
   getFollows(userID) {
-    // Your code here
+    return this.follows[userID];
   }
 
   getFollowers(userID) {
-    // Your code here
+    const followers = new Set();
+    for (const user in this.follows) {
+      if (this.follows[user].has(userID)) {
+        followers.add(Number(user));
+      }
+    }
+
+    return followers;
   }
 
   getRecommendedFollows(userID, degrees) {
